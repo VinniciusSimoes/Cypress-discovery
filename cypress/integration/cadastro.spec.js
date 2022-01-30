dcf18012ebddfb36a3f285166178e48882db5d00
Vinnicius Simoes <vinniciussilva7@gmail.com>
2022-01-30 08:55:02 -0300

    Atualizando o arquivo

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,10 +1,8 @@
+import SignupPage from '../pages/SignupPage'
+
 describe('Cadastro', ()=>{
     it('Usuário deve se tornar um entregador', ()=> {
-        cy.viewport(1440, 900)                    // ajusta o tamanho da tela
-        cy.visit('https://buger-eats.vercel.app')// acessa a página
 
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas') // isso é somente um checkpoint
 
         var deliver = {
             name: 'Vinnicius Simões',
@@ -23,38 +21,17 @@ describe('Cadastro', ()=>{
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name);
-        cy.get('input[name="cpf"]').type(deliver.cpf);
-        cy.get('input[name="email"]').type(deliver.email);
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode"').type(deliver.address.postalcode);
-        cy.get('input[type=button][value="Buscar CEP"]').click();
-
-        cy.get('input[name=address-number]').type(deliver.address.number);
-        cy.get('input[name=address-details]').type(deliver.address.details);
-
-        cy.get('input[name="address"]').should('have.value',deliver.address.street);
-        cy.get('input[name="district"]').should('have.value',deliver.address.district);
-        cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state);
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click();
-
-        cy.get('input[accept^="image"]').attachFile('/images/'+ deliver.cnh);
-
-        cy.get('form button[type="submit"]').click();
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
-
-        cy.get('.swal2-container div[class="swal2-html-container"]').should('have.text', expectedMessage);
+        signup.modalContentShouldBe(expectedMessage)
     });
 
     it('CPF incorreto', ()=> {
-        cy.viewport(1440, 900)                    // ajusta o tamanho da tela
-        cy.visit('https://buger-eats.vercel.app')// acessa a página
-
-        cy.get('a[href="/deliver"]').click()
-        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas') // isso é somente um checkpoint
 
         var deliver = {
             name: 'Vinnicius Simões',
@@ -73,27 +50,13 @@ describe('Cadastro', ()=>{
             cnh: 'cnh-digital.jpg'
         }
 
-        cy.get('input[name="name"]').type(deliver.name);
-        cy.get('input[name="cpf"]').type(deliver.cpf);
-        cy.get('input[name="email"]').type(deliver.email);
-        cy.get('input[name="whatsapp"]').type(deliver.whatsapp);
+        var signup = new SignupPage()
 
-        cy.get('input[name="postalcode"').type(deliver.address.postalcode);
-        cy.get('input[type=button][value="Buscar CEP"]').click();
+        signup.go()
+        signup.fillForm(deliver)
+        signup.submit()
 
-        cy.get('input[name=address-number]').type(deliver.address.number);
-        cy.get('input[name=address-details]').type(deliver.address.details);
+        signup.alertMessageShouldBe('Oops! CPF inválido')
 
-        cy.get('input[name="address"]').should('have.value',deliver.address.street);
-        cy.get('input[name="district"]').should('have.value',deliver.address.district);
-        cy.get('input[name="city-uf"]').should('have.value',deliver.address.city_state);
-
-        cy.contains('.delivery-method li', deliver.delivery_method).click();
-
-        cy.get('input[accept^="image"]').attachFile('/images/'+ deliver.cnh);
-
-        cy.get('form button[type="submit"]').click();
-
-        cy.get('.alert-error').should('have.text','Oops! CPF inválido');
     });
 });
