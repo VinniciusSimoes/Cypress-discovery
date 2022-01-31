35707e3daa64559b01448b8c0c4b42fa89f6e7c1
VinniciusSimoes <vinniciussilva7@gmail.com>
2022-01-31 19:53:46 -0300

    Atualizando mais conteudos

--- cypress/integration/signup.spec.js
+++ cypress/integration/signup.spec.js
@@ -1,16 +1,20 @@
 import signup from '../pages/SignupPage'
+import signupFactory from '../factories/SignupFactory'
 
 describe('Signup', () => {
 
-    beforeEach(function () {
-        cy.fixture('deliver').then((d) => {    // d eh a massa de testes , coloco um argumento
-            this.deliver = d    // uso o this para acessar a informação para os casos , ela é uma palavra reservada para criar uma variavel de contexto
-        })
-    });
+    // beforeEach(function () {
+    //     cy.fixture('deliver').then((d) => {    // d eh a massa de testes , coloco um argumento
+    //         this.deliver = d    // uso o this para acessar a informação para os casos , ela é uma palavra reservada para criar uma variavel de contexto
+    //     })
+    // });
 
     it('User should be deliver', function () {
+
+        var deliver = signupFactory.deliver()
+
         signup.go()
-        signup.fillForm(this.deliver.signup)
+        signup.fillForm(deliver)
         signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
@@ -18,8 +22,13 @@ describe('Signup', () => {
     });
 
     it('Incorrect document', function () {
+
+        var deliver = signupFactory.deliver()
+
+        deliver.cpf = '000000000AA'
+
         signup.go()
-        signup.fillForm(this.deliver.cpf_inv)
+        signup.fillForm(deliver)
         signup.submit()
 
         signup.alertMessageShouldBe('Oops! CPF inválido')
@@ -27,8 +36,13 @@ describe('Signup', () => {
     });
 
     it('Incorrect email', function () {
+
+        var deliver = signupFactory.deliver()
+
+        deliver.email = 'user.com.br'
+
         signup.go()
-        signup.fillForm(this.deliver.email_inv)
+        signup.fillForm(deliver)
         signup.submit()
 
         signup.alertMessageShouldBe('Oops! Email com formato inválido.')
